ce1a991c968fdd3411695169d27b8e796d7528ca
Leonardo Hahn <leonardo.hahn@tfgco.com>
2018-11-01 12:22:41 -0300

    fix enum

--- src/v3/AsyncKeepAliveResponse.cpp
+++ src/v3/AsyncKeepAliveResponse.cpp
@@ -3,7 +3,7 @@
 etcdv3::AsyncKeepAliveResponse::AsyncKeepAliveResponse(LeaseKeepAliveResponse const &resp)
 {
     revision = resp.header().revision();
-    status.etcd_error_code = status.etcd_error_message.empty() ? StatusCode::OK : StatusCode::OTHER_ERROR;
+    status.etcd_error_code = status.etcd_error_message.empty() ? V3StatusCode::OK : V3StatusCode::OTHER_ERROR;
     value.kvs.set_lease(resp.id());
     value.set_ttl(resp.ttl());
 }
