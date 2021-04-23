042c487a603248d0d6289393541c37efd14132a6
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2021-04-23 20:25:05 +0100

    Hardcode revision to 0 to always watch the latest revision

--- src/Watcher.cpp
+++ src/Watcher.cpp
@@ -42,27 +42,27 @@ etcd::Watcher::Watcher(
   , callback(callback)
   , isCancelled(false)
 {
-  auto fromRevisionExplicit = fromRevision;
-  if (fromRevisionExplicit == 0)
-  {
-    const auto stub = etcdserverpb::KV::NewStub(channel);
-    etcdv3::ActionParameters get_params;
-    get_params.key.assign(key);
-    get_params.withPrefix = recursive;
-    get_params.kv_stub = stub.get();
-    auto get_call = std::make_shared<etcdv3::AsyncGetAction>(std::move(get_params));
-    get_call->waitForResponse();
-    fromRevisionExplicit = get_call->ParseResponse().revision;
-    if (fromRevisionExplicit == 0)
-    {
-      throw watch_error("cannot acquire current etcd key-value store revision");
-    }
-  }
+  //auto fromRevisionExplicit = fromRevision;
+  //if (fromRevisionExplicit == 0)
+  //{
+    //const auto stub = etcdserverpb::KV::NewStub(channel);
+    //etcdv3::ActionParameters get_params;
+    //get_params.key.assign(key);
+    //get_params.withPrefix = recursive;
+    //get_params.kv_stub = stub.get();
+    //auto get_call = std::make_shared<etcdv3::AsyncGetAction>(std::move(get_params));
+    //get_call->waitForResponse();
+    //fromRevisionExplicit = get_call->ParseResponse().revision;
+    //if (fromRevisionExplicit == 0)
+    //{
+      //throw watch_error("cannot acquire current etcd key-value store revision");
+    //}
+  //}
   watch_action_parameters.key = key;
   watch_action_parameters.withPrefix = recursive;
   watch_action_parameters.watch_stub = watchServiceStub.get();
   // if fromRevision is 0, interested revision for user is current + 1
-  watch_action_parameters.revision = fromRevisionExplicit + (fromRevision == 0 ? 1 : 0);
+  watch_action_parameters.revision = 0;
   try
   {
     doWatch();
